125d887879c89caee35fe4991b528ad4dc34c73e
Lauren <lburke522@gmail.com>
2022-09-19 16:25:52 -0400

    started connection on index

--- db/index.js
+++ db/index.js
@@ -1,2 +1,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
